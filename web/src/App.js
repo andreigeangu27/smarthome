66486e685fbdcfb6043ea6d813c50d213abdb436
Andrei Geangu <andrei.geangucopil@gmail.com>
2022-06-22 16:25:09 +0300

    add new changes

--- web/src/App.js
+++ web/src/App.js
@@ -2,21 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const faCeva = () => {
+    alert('SALUT')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Smart home Andrei Geangu  
+       <button onClick={faCeva}>APASAMA</button>
+       <ul>
+        <li>CEVa</li>
+        <li>CEVa</li>
+       </ul>
       </header>
     </div>
   );
